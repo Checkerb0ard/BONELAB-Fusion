b75446182262ca4adec945ee41452cb756146bf8
Logan Shockley <loganv182@gmail.com>
2026-08-10 21:34:00 -0500

    Added specific platform getter

--- LabFusion/src/Utilities/PlatformHelper.cs
+++ LabFusion/src/Utilities/PlatformHelper.cs
@@ -1,22 +1,68 @@
-﻿using MelonLoader;
+﻿using Il2CppSLZ.Marrow.Data;
+
+using MelonLoader;
 
 namespace LabFusion.Utilities;
 
+/// <summary>
+/// Helper for determining information about the current platform that the game is running on.
+/// </summary>
 public static class PlatformHelper
 {
-    private static readonly bool _isAndroidCached = MelonUtils.CurrentPlatform == (MelonPlatformAttribute.CompatiblePlatforms)3;
+    /// <summary>
+    /// The platforms the game can be on.
+    /// </summary>
+    public enum Platform
+    {
+        /// <summary>
+        /// No valid platform was found.
+        /// </summary>
+        None,
+
+        /// <summary>
+        /// The steam store.
+        /// </summary>
+        Steam,
+
+        /// <summary>
+        /// The meta store on PCVR.
+        /// </summary>
+        MetaPCVR,
+
+        /// <summary>
+        /// The meta store on standalone Quest.
+        /// </summary>
+        MetaQuest
+    }
+
+    /// <summary>
+    /// Returns true if the game is running on android.
+    /// </summary>
     public static bool IsAndroid => _isAndroidCached;
 
-    public const string AndroidName = "QUEST";
-    public const string WindowsName = "PC";
+    private static readonly bool _isAndroidCached = MelonUtils.CurrentPlatform == (MelonPlatformAttribute.CompatiblePlatforms)3;
 
-    public static string GetPlatformName()
+    /// <summary>
+    /// Gets the platform the game is currently running on.
+    /// </summary>
+    /// <returns></returns>
+    public static Platform GetPlatform()
     {
-        if (IsAndroid)
+        var localData = PlatformSelectionData.LocalData;
+
+        if (localData == null)
         {
-            return AndroidName;
+            return Platform.None;
         }
 
-        return WindowsName;
+        var vrPlatform = localData.platform;
+
+        return vrPlatform switch
+        {
+            VRPlatform.Steam => Platform.Steam,
+            VRPlatform.OculusHome => Platform.MetaPCVR,
+            VRPlatform.OculusQuest => Platform.MetaQuest,
+            _ => Platform.None,
+        };
     }
 }
