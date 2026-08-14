2de53837a895da54b13aa1646db39c205bd65b89
Logan Shockley <loganv182@gmail.com>
2026-08-14 00:02:12 -0500

    Added more dedicated server buttons

--- DedicatedServerSupport/DedicatedServerGUI.cs
+++ DedicatedServerSupport/DedicatedServerGUI.cs
@@ -1,4 +1,7 @@
-﻿using UnityEngine;
+﻿using LabFusion.Network;
+using LabFusion.Player;
+
+using UnityEngine;
 
 namespace MarrowFusion.DedicatedServer;
 
@@ -6,6 +9,75 @@ public static class DedicatedServerGUI
 {
     public static void OnGUI()
     {
+        if (!NetworkLayerManager.IsLoggedIn)
+        {
+            OnLoggedOutGUI();
+            return;
+        }
+
+        if (NetworkLayerManager.IsLoggingIn)
+        {
+            OnLoggingInGUI();
+            return;
+        }
+
+        OnLoggedInGUI();
+    }
+
+    private static void OnLoggedOutGUI()
+    {
+        foreach (var layer in NetworkLayerManager.SupportedLayers)
+        {
+            if (GUILayout.Button($"Log In To {layer.Title}"))
+            {
+                NetworkLayerManager.LogIn(layer);
+            }
+        }
+    }
+
+    private static void OnLoggingInGUI()
+    {
+        GUILayout.Button("Logging in...");
+    }
+
+    private static void OnLoggedInGUI()
+    {
+        if (ServerManager.IsServerRunning)
+        {
+            OnServerGUI();
+        }
+        else
+        {
+            OnNoServerGUI();
+        }
+
+        bool logOut = GUILayout.Button("Log Out");
+
+        if (logOut)
+        {
+            NetworkLayerManager.LogOut();
+        }
+    }
+
+    private static void OnNoServerGUI()
+    {
         bool startServer = GUILayout.Button("Start Server");
+
+        if (startServer)
+        {
+            ServerManager.StartServer();
+        }
+    }
+
+    private static void OnServerGUI()
+    {
+        GUILayout.Label($"{PlayerIDManager.PlayerCount} Players");
+
+        bool stopServer = GUILayout.Button("Stop Server");
+
+        if (stopServer)
+        {
+            ServerManager.StopServer();
+        }
     }
 }
